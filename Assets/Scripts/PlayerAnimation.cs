9791f5daaa26e31dfa162e9e69cb76ec64423034
EricXwh <ericxiewh@163.com>
2023-09-28 00:04:25 -0400

    add animation

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
